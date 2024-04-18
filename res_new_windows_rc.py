6596a357d9b770176d58812e10f82ec1dc8e2c9b
Alenwar <olhovskaya.alena@gmail.com>
2024-04-18 17:03:16 +0200

    Add files

---
+++ res_new_windows_rc.py
@@ -0,0 +1,132 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.6.1
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x02\xc0\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4\x22 viewBox=\x220 -9\
+60 960 960\x22 widt\
+h=\x2224\x22><path d=\x22\
+M610-210q12 0 21\
+-9t9-21q0-12-9-2\
+1t-21-9q-12 0-21\
+ 9t-9 21q0 12 9 \
+21t21 9Zm110 0q1\
+2 0 21-9t9-21q0-\
+12-9-21t-21-9q-1\
+2 0-21 9t-9 21q0\
+ 12 9 21t21 9Zm1\
+10 0q12 0 21-9t9\
+-21q0-12-9-21t-2\
+1-9q-12 0-21 9t-\
+9 21q0 12 9 21t2\
+1 9Zm-630 90q-33\
+ 0-56.5-23.5T120\
+-200v-560q0-33 2\
+3.5-56.5T200-840\
+h560q33 0 56.5 2\
+3.5T840-760v268q\
+-19-9-39-15.5t-4\
+1-9.5v-243H200v5\
+60h242q3 22 9.5 \
+42t15.5 38H200Zm\
+0-120v40-560 243\
+-3 280Zm80-40h16\
+3q3-21 9.5-41t14\
+.5-39H280v80Zm0-\
+160h244q32-30 71\
+.5-50t84.5-27v-3\
+H280v80Zm0-160h4\
+00v-80H280v80ZM7\
+20-40q-83 0-141.\
+5-58.5T520-240q0\
+-83 58.5-141.5T7\
+20-440q83 0 141.\
+5 58.5T920-240q0\
+ 83-58.5 141.5T7\
+20-40Z\x22/></svg>\
+\x00\x00\x01\xdc\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4\x22 viewBox=\x220 -9\
+60 960 960\x22 widt\
+h=\x2224\x22><path d=\x22\
+M360-200v-80h480\
+v80H360Zm0-240v-\
+80h480v80H360Zm0\
+-240v-80h480v80H\
+360ZM200-160q-33\
+ 0-56.5-23.5T120\
+-240q0-33 23.5-5\
+6.5T200-320q33 0\
+ 56.5 23.5T280-2\
+40q0 33-23.5 56.\
+5T200-160Zm0-240\
+q-33 0-56.5-23.5\
+T120-480q0-33 23\
+.5-56.5T200-560q\
+33 0 56.5 23.5T2\
+80-480q0 33-23.5\
+ 56.5T200-400Zm0\
+-240q-33 0-56.5-\
+23.5T120-720q0-3\
+3 23.5-56.5T200-\
+800q33 0 56.5 23\
+.5T280-720q0 33-\
+23.5 56.5T200-64\
+0Z\x22/></svg>\
+"
+
+qt_resource_name = b"\
+\x00\x04\
+\x00\x06\xfa^\
+\x00i\
+\x00c\x00o\x00n\
+\x00\x05\
+\x00O\xa6S\
+\x00I\
+\x00c\x00o\x00n\x00s\
+\x00.\
+\x07@C'\
+\x00o\
+\x00t\x00h\x00e\x00r\x00_\x00a\x00d\x00m\x00i\x00s\x00s\x00i\x00o\x00n\x00_\x00F\
+\x00I\x00L\x00L\x000\x00_\x00w\x00g\x00h\x00t\x004\x000\x000\x00_\x00G\x00R\x00A\
+\x00D\x000\x00_\x00o\x00p\x00s\x00z\x002\x004\x00.\x00s\x00v\x00g\
+\x003\
+\x01\xd8\x94\xe7\
+\x00f\
+\x00o\x00r\x00m\x00a\x00t\x00_\x00l\x00i\x00s\x00t\x00_\x00b\x00u\x00l\x00l\x00e\
+\x00t\x00e\x00d\x00_\x00F\x00I\x00L\x00L\x000\x00_\x00w\x00g\x00h\x00t\x004\x000\
+\x000\x00_\x00G\x00R\x00A\x00D\x000\x00_\x00o\x00p\x00s\x00z\x002\x004\x00.\x00s\
+\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x0e\x00\x02\x00\x00\x00\x02\x00\x00\x00\x03\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x80\x00\x00\x00\x00\x00\x01\x00\x00\x02\xc4\
+\x00\x00\x01\x8c\xdc\x1b\xf24\
+\x00\x00\x00\x1e\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x8c\xdc\x1bs\x88\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
